344fea218b534cc331ef62dcc517df512f2efb45
Jonathan Adams <jonnoadams@gmail.com>
2017-03-28 16:02:20 -0500

    Altered Initial MsgBox

--- Validation_Form/Forms/5. Health Maint/Health_Maint_Setup.vb
+++ Validation_Form/Forms/5. Health Maint/Health_Maint_Setup.vb
@@ -16,125 +16,126 @@ Application.EnableEvents = False
 
 Sheets("Validated Mappings").Select
 
-MsgBox ("Program is about to run. Please leave computer alone until completed")
+MsgBox ("Program is about to run. This will take about a minute to complete. Please leave computer alone until completed")
 
 ActiveSheet.AutoFilterMode = False 'Disables autoFilter
+
 'If table exists on sheet then convert to range
-    If ActiveSheet.ListObjects.Count > 0 Then
+If ActiveSheet.ListObjects.Count > 0 Then
 
-        With ActiveSheet.ListObjects(1)
-            Set rList = .Range
-            .Unlist
-        End With
-
-        With rList
-            .Interior.ColorIndex = xlColorIndexNone
-            .Font.ColorIndex = xlColorIndexAutomatic
-            .Borders.LineStyle = xlLineStyleNone
-        End With
-
-    End If
-
-    Set sht = Worksheets("Validated Mappings")
-    Set StartCell = Range("A1")
-
-'Refresh UsedRange
-    Worksheets("Validated Mappings").UsedRange
-
-'Find Last Row and Column
-    LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
-    LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
-
-'Select Range
-    sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
-
-'Turn selected Range Into Table
-    Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
-    tbl.Name = "Validated_tbl"
-    tbl.TableStyle = "TableStyleLight12"
-
-  Application.Goto Reference:="Validated_tbl"
-    Set Rng = Selection
-
-  For Each cell In Rng 'Loops through cells in range
-    If IsNumeric(cell) Then
-      cell.Value = Val(cell.Value)
-      cell.NumberFormat = "0"
-    End If
-  Next cell
-
-  Sheets("Health Maint Clinical Events").Select
-
-  ActiveSheet.AutoFilterMode = False 'Disables autoFilter
-
-  'If table exists on sheet then convert to range
-    If ActiveSheet.ListObjects.Count > 0 Then
-
-        With ActiveSheet.ListObjects(1)
-            Set rList = .Range
-            .Unlist
-        End With
-
-        With rList
-            .Interior.ColorIndex = xlColorIndexNone
-            .Font.ColorIndex = xlColorIndexAutomatic
-            .Borders.LineStyle = xlLineStyleNone
-        End With
-
-    End If
-
-    Set sht = Worksheets("Health Maint Clinical Events")
-    Set StartCell = Range("A1")
-
-  'Refresh UsedRange
-    Worksheets("Health Maint Clinical Events").UsedRange
-
-  'Find Last Row and Column
-    LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
-    LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
-
-  'Select Range
-    sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
-
-  'Turn selected Range Into Table
-    Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
-    tbl.Name = "Health_Maint_Tbl"
-    tbl.TableStyle = "TableStyleLight12"
-
-    Application.Goto Reference:="Health_Maint_Tbl"
-    Set Rng = Selection
-
-    For Each cell In Rng 'Loops through cells in range
-      If IsNumeric(cell) Then
-        cell.Value = Val(cell.Value)
-        cell.NumberFormat = "0"
-      End If
-    Next cell
-
-    Range("A2").Select
-    ActiveCell = "=IFERROR(INDEX('Validated Mappings'!I:I,MATCH(BI2,'Validated Mappings'!D:D,0)),0)"
-        Selection.AutoFill Destination:=Range("Health_Maint_Tbl[Mapped?]")
-
-    'Re-enables Auto-calculate for forumlas
-        Application.Calculation = xlCalculationAutomatic
-
-    Columns("A:A").Select
-    With Selection
-      .HorizontalAlignment = xlCenter
-      .VerticalAlignment = xlCenter
-      .Orientation = 0
-      .AddIndent = False
-      .IndentLevel = 0
-      .ShrinkToFit = False
-      .ReadingOrder = xlContext
-      .MergeCells = False
+    With ActiveSheet.ListObjects(1)
+        Set rList = .Range
+        .Unlist
     End With
 
-    Range("A2").Select
+    With rList
+        .Interior.ColorIndex = xlColorIndexNone
+        .Font.ColorIndex = xlColorIndexAutomatic
+        .Borders.LineStyle = xlLineStyleNone
+    End With
 
-    Application.ScreenUpdating = True
-    Application.EnableEvents = True
+End If
 
-    MsgBox ("Program Completed")
+Set sht = Worksheets("Validated Mappings")
+Set StartCell = Range("A1")
+
+'Refresh UsedRange
+Worksheets("Validated Mappings").UsedRange
+
+'Find Last Row and Column
+LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
+LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
+
+'Select Range
+sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
+
+'Turn selected Range Into Table
+  Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
+  tbl.Name = "Validated_tbl"
+  tbl.TableStyle = "TableStyleLight12"
+
+Application.Goto Reference:="Validated_tbl"
+  Set Rng = Selection
+
+For Each cell In Rng 'Loops through cells in range
+  If IsNumeric(cell) Then
+    cell.Value = Val(cell.Value)
+    cell.NumberFormat = "0"
+  End If
+Next cell
+
+Sheets("Health Maint Clinical Events").Select
+
+ActiveSheet.AutoFilterMode = False 'Disables autoFilter
+
+'If table exists on sheet then convert to range
+If ActiveSheet.ListObjects.Count > 0 Then
+
+    With ActiveSheet.ListObjects(1)
+        Set rList = .Range
+        .Unlist
+    End With
+
+    With rList
+        .Interior.ColorIndex = xlColorIndexNone
+        .Font.ColorIndex = xlColorIndexAutomatic
+        .Borders.LineStyle = xlLineStyleNone
+    End With
+
+End If
+
+Set sht = Worksheets("Health Maint Clinical Events")
+Set StartCell = Range("A1")
+
+'Refresh UsedRange
+Worksheets("Health Maint Clinical Events").UsedRange
+
+'Find Last Row and Column
+LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
+LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
+
+'Select Range
+sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
+
+'Turn selected Range Into Table
+Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
+tbl.Name = "Health_Maint_Tbl"
+tbl.TableStyle = "TableStyleLight12"
+
+Application.Goto Reference:="Health_Maint_Tbl"
+Set Rng = Selection
+
+For Each cell In Rng 'Loops through cells in range
+  If IsNumeric(cell) Then
+    cell.Value = Val(cell.Value)
+    cell.NumberFormat = "0"
+  End If
+Next cell
+
+Range("A2").Select
+ActiveCell = "=IFERROR(INDEX('Validated Mappings'!I:I,MATCH(BI2,'Validated Mappings'!D:D,0)),0)"
+    Selection.AutoFill Destination:=Range("Health_Maint_Tbl[Mapped?]")
+
+'Re-enables Auto-calculate for forumlas
+    Application.Calculation = xlCalculationAutomatic
+
+Columns("A:A").Select
+With Selection
+  .HorizontalAlignment = xlCenter
+  .VerticalAlignment = xlCenter
+  .Orientation = 0
+  .AddIndent = False
+  .IndentLevel = 0
+  .ShrinkToFit = False
+  .ReadingOrder = xlContext
+  .MergeCells = False
+End With
+
+Range("A2").Select
+
+Application.ScreenUpdating = True
+Application.EnableEvents = True
+
+MsgBox ("Program Completed")
 
 End Sub
