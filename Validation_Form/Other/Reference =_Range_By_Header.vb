440b5689a734c9f659b888b839e46db972678d0c
Jonathan Adams <jonnoadams@gmail.com>
2017-03-31 13:28:48 -0500

    Find a columns position by its header

--- Validation_Form/Other/Reference =_Range_By_Header.vb
+++ Validation_Form/Other/Reference =_Range_By_Header.vb
@@ -1,20 +1,16 @@
 'The below example searches for the header based on the name. In the example, it colors the entire 'column Red based on match, but you can change that. The variables should be easy to follow, but 'please let me know of questions.
 
 Sub Color_Range_Based_On_Header()
-    Dim rngHeaders As Range
-    Dim rngHdrFound As Range
 
-    Const ROW_HEADERS As Integer = 1
-    Const HEADER_NAME As String = "Location"
+  Dim Sheet_Headers As Variant
+  Dim rngDateHeader As range
+  Dim rngHeaders As range
 
-    Set rngHeaders = Intersect(Worksheets("Sheet1").UsedRange, Worksheets("Sheet1").Rows(ROW_HEADERS))
-    Set rngHdrFound = rngHeaders.Find(HEADER_NAME)
 
-    If rngHdrFound Is Nothing Then
-        'Do whatever you want if the header is missing
-        Exit Sub
-    End If
+  Set rngHeaders = range("1:1")
+  Set rngDateHeader = rngHeaders.Find("Event Code Mapped?")
 
-    Range(rngHdrFound, rngHdrFound.End(xlDown)).Interior.Color = vbRed
+  Columnlocation = Mid(rngDateHeader.Address, 2, 1)
+
 
 End Sub
