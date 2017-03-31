f54546557d1bf7823d993b670bdaa8d6193b37e9
Jonathan Adams <jonnoadams@gmail.com>
2017-03-31 11:06:14 -0500

    Updated with using Array format

--- Validation_Form/Other/Temp2.vb
+++ Validation_Form/Other/Temp2.vb
@@ -1,42 +1,140 @@
-Sub ArrayTest()
 
-  Dim sht As Worksheet
-  Dim LastRow As Long
-  Dim LastColumn As Long
-  Dim StartCell As range
-  Dim Sheet As Worksheet
-  Dim Sheet_Name As String
-  Dim WkNames As Variant
-  Dim TblNames As Variant
+    Sub Testing()
 
-  Application.Calculation = xlCalculationManual
-  Application.EnableEvents = False
+    '
+    'This code will take values from a table and put them in an arrao.
+    'Then it Will perform changes to the data within the array and then write the array back to the sheet.
+    'This changes the values all at once instead of one at a time.
+    '
+    '
 
-  WkNames = Array("Results", "Validation Sheet", "Validated Mappings")
-  TblNames = Array("Results_Tbl", "Val_Tbl", "Mappings_Tbl")
+      Dim DataRange As Variant 'Declare array variable
+      Dim Irow As Long 'The row variable
+      Dim Icol As Integer 'The column variable if you need to loop through multiple columns
+      Dim DocType As Variant 'Variable used to store column value
+      Dim ControlArray As Variant
+      Dim ControlTypeCheck As Variant
+      Dim Nomenclature_Val_Check As Variant
+      Dim EventCode_Val_Check As Variant
+      Dim sht As Worksheet
+      Dim LastRow As Long
+      Dim LastColumn As Long
+      Dim StartCell As Range
+      Dim Sheet As Worksheet
+      Dim rList As Range
+      Dim Confirm_Run As Integer
 
-  For i = 0 To UBound(WkNames)
+      'Disables settings to improve performance
+      Application.ScreenUpdating = False
+      Application.Calculation = xlCalculationManual
+      Application.EnableEvents = False
 
-    Sheets(WkNames(i)).Select
+      'Prompts user to confirm they have reviewed the data in the validation form BEFORE running this.
+      Confirm_Run = MsgBox("This program will populate the notes and team fields for the nomenclature data. Click ""Ok"" to run or ""Cancel"" to cancel the program.", vbOkCancel + vbQuestion, "Empty Sheet")
 
-    Set sht = Worksheets(WkNames(i)) 'Sets value
-    Set StartCell = range("A1") 'Start cell used to determine where to begin creating the table range
+      'If user hits cancel then close program.
+      If Confirm_Run = vbCancel Then
+        MsgBox ("Program is canceling per user action.")
+        Exit Sub
+      End If
 
-    'Find Last Row and Column
+      ActiveSheet.AutoFilterMode = False 'Removes filters from sheet
 
-    LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
-    LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
-    Sheet_Name = WkNames(i) 'Assigns sheet name to a variable as a string
+      If ActiveSheet.ListObjects.Count > 0 Then
 
-    'Select Range
-    sht.range(StartCell, sht.Cells(LastRow, LastColumn)).Select
+        With ActiveSheet.ListObjects(1)
+            Set rList = .Range
+            .Unlist                           ' convert the table back to a range
+        End With
 
-    'Creates the table
-    Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
-    tbl.Name = TblNames(i) 'Names the table
-    tbl.TableStyle = "TableStyleLight12" 'Sets table color theme
-    Columns.AutoFit 'Autofits columns on sheet
+        With rList
+            .Interior.ColorIndex = xlColorIndexNone
+            .Font.ColorIndex = xlColorIndexAutomatic
+            .Borders.LineStyle = xlLineStyleNone
+        End With
 
-  Next
+      End If
 
-End Sub
+
+        Set sht = ActiveSheet 'Sets value
+        Set StartCell = Range("A1") 'Start cell used to determine where to begin creating the table range
+
+      'Find Last Row and Column
+        LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
+        LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
+
+      'Select Range
+        sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
+
+      'Creates the table
+        Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
+        tbl.Name = "New_Lines" 'Names the table
+        tbl.TableStyle = "TableStyleLight12" 'Sets table color theme
+
+        Rows("1:1").Select
+        With Selection.Font
+          .ThemeColor = xlThemeColorDark1
+          .TintAndShade = 0
+        End With
+
+        'Creates named Range starting at column E
+        Range("E2:T2").Select
+        Range(Selection, Selection.End(xlDown)).Select
+
+        Selection.Name = "Data_Range"
+
+
+      'Array to check DocumentType
+      ControlArray = Array("Alpha List", "Alpha Combo", "Discrete Grid", "UltraGrid", "PowerGrid", "Multi")
+
+      'Saves range to array
+      DataRange = range("Data_Range").Value 'writes the named data range to the array variable
+
+        For Irow = 1 To UBound(DataRange) 'Loops through all rows within the range.
+          DocType = DataRange(Irow, 1) 'Assigns current value to a variable
+          ControlTypeCheck = DataRange(Irow, 9)
+          Nomenclature_Val_Check = DataRange(Irow, 16)
+          EventCode_Val_Check = DataRange(Irow, 15)
+
+          'Checks if control type is within the array.
+          IsInArray = Not IsError(Application.Match(ControlTypeCheck, ControlArray, 0))
+
+          If IsInArray = TRUE _
+            And Nomenclature_Val_Check = "0" _
+            And EventCode_Val_Check = "0" _
+            Then
+
+            DataRange(Irow, 13) = "This nomenclature and event code are not mapped and should be if this will be used to complete the measure."
+            DataRange(IRow, 14) = "PCST"
+
+
+          ElseIf IsInArray = TRUE _
+            And Nomenclature_Val_Check = "Validated" _
+            And EventCode_Val_Check = "0" _
+            Then
+
+            DataRange(Irow, 13) = "This nomenclature is mapped but the event code will need to be mapped if this will be used to complete the measure."
+            DataRange(IRow, 14) = "PCST"
+
+          ElseIf IsInArray = TRUE _
+            And Nomenclature_Val_Check = "0" _
+            And EventCode_Val_Check = "Validated" _
+            Then
+
+            DataRange(Irow, 13) = "This event code is mapped but the nomenclature is not mapped and should be if this will be used to complete the measure."
+            DataRange(IRow, 14) = "Consulting"
+
+          End If
+
+        Next Irow
+
+
+      'Write the updated DataRange Array to the excel file
+      range("Data_Range").Value = DataRange
+
+      're-enables settings previously disabled
+      Application.ScreenUpdating = True
+      Application.Calculation = xlCalculationAutomatic
+      Application.EnableEvents = True
+
+    End Sub
