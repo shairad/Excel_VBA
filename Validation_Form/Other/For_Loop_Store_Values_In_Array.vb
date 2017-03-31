52a4d14e13a2b128fc93ef733dd1454949fcd4a1
Jonathan Adams <jonnoadams@gmail.com>
2017-03-31 10:20:56 -0500

    For loop stored array

---
+++ Validation_Form/Other/For_Loop_Store_Values_In_Array.vb
@@ -0,0 +1,28 @@
+Sub Testing()
+
+'
+'This code will take values from a table and put them in an arrao.
+'Then it Will perform changes to the data within the array and then write the array back to the sheet.
+'This changes the values all at once instead of one at a time.
+'
+'
+
+
+  Dim DataRange As Variant 'Declare array variable
+  Dim Irow As Long 'The row variable
+  Dim Icol As Integer 'The column variable if you need to loop through multiple columns
+  Dim MyType As Variant 'Variable used to store column value
+
+
+  DataRange = range("Data_Range").Value 'writes the named data range to the array variable
+
+  For Irow = 1 To UBound(DataRange) 'Loops through all rows within the range.
+      MyType = DataRange(Irow, 1) 'Assigns current value to a variable
+
+      If MyType = "PowerForm" Then
+        DataRange(Irow, 13) = "Yes" 'If value is true, then update value in row IView, Column 13 to "X"
+      End If
+  Next Irow
+  range("Data_Range").Value = DataRange 'Write the updated DataRange Array to the excel file
+
+End Sub
--- Validation_Form/Other/Temp.vb
+++ Validation_Form/Other/Temp.vb
@@ -1,74 +1,22 @@
-Sub SpecialLoop()
+Sub Testing()
+Dim DataRange As Variant
+Dim Irow As Long
+Dim Icol As Integer
+Dim MyType As Variant
+Dim MyNote As Variant
 
-    Dim sht As Worksheet
-    Dim LastRow As Long
-    Dim LastColumn As Long
-    Dim StartCell As Range
-    Dim Sheet As Worksheet
-    Dim rList As Range
 
-    ActiveSheet.AutoFilterMode = False 'Removes filters from sheet
+DataRange = Range("Data_Range").Value
 
-    If ActiveSheet.ListObjects.Count > 0 Then
+For Irow = 1 To UBound(DataRange)
+  For Icol = 1
+    MyType = DataRange(Irow, Icol)
 
-    With ActiveSheet.ListObjects(1)
-        Set rList = .Range
-        .Unlist                           ' convert the table back to a range
-    End With
-
-    With rList
-        .Interior.ColorIndex = xlColorIndexNone
-        .Font.ColorIndex = xlColorIndexAutomatic
-        .Borders.LineStyle = xlLineStyleNone
-    End With
-
-    Rows("1:1").Select
-    With Selection.Font
-      .ThemeColor = xlThemeColorDark1
-      .TintAndShade = 0
-    End With
-
+    If MyType =  "PowerForm" Then
+      DataRange(Irow, 13) = "Yes"
     End If
-
-
-      Set sht = ActiveSheet 'Sets value
-      Set StartCell = Range("A1") 'Start cell used to determine where to begin creating the table range
-
-    'Find Last Row and Column
-      LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
-      LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
-
-    'Select Range
-      sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
-
-    'Creates the table
-      Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
-      tbl.Name = "New_Lines" 'Names the table
-      tbl.TableStyle = "TableStyleLight12" 'Sets table color theme
-
-
-
-
-      ActiveSheet.ListObjects("New_Lines").Range.AutoFilter Field:=5, Criteria1:= _
-          "=PowerForm", Operator:=xlOr, Criteria2:="=IView"
-      ActiveWindow.SmallScroll Down:=-6
-      ActiveSheet.ListObjects("New_Lines").Range.AutoFilter Field:=13, Criteria1:= _
-          Array("Alpha List", "Alpha Combo", "Discrete Grid", "UltraGrid", "PowerGrid", "Multi"), Operator:=xlFilterValues
-      ActiveSheet.ListObjects("New_Lines").Range.AutoFilter Field:=19, Criteria1:= _
-          "0"
-      ActiveSheet.ListObjects("New_Lines").Range.AutoFilter Field:=20, Criteria1:= _
-          "0"
-
-
-
-      '''Loops Through filtered rows'''''
-      Range("R3:R" & Cells.SpecialCells(xlCellTypeLastCell).Row).Select
-      Selection.Name = "Visible_Range"
-
-      'For each visible cell within Range'
-      For Each cell In Range("Visible_Range").SpecialCells(xlCellTypeVisible)
-          cell.Value = "This nomenclature is mapped but the event code will need to be mapped if this will be used to complete the measure."
-          cell.Offset(0,1).value = "PCST"
-      Next
+  Next Icol
+Next Irow
+Range("Data_Range").Value = DataRange
 
 End Sub
