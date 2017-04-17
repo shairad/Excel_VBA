107428b49f7152cd31bf9eeb969441dcf8462abc
Jonnokc <jonnoadams@gmail.com>
2017-04-17 09:35:06 -0500

    Fixed cleanup code bugs

--- Validation_Form/Summary Page Code/Summary_Cleanup.vb
+++ Validation_Form/Summary Page Code/Summary_Cleanup.vb
@@ -1,4 +1,4 @@
-Sub Summary_Cleanup()
+Private Sub Summary_Cleanup()
 
 ' Delets extra sheets and columns after summary sheet process has completed
 
@@ -8,16 +8,16 @@ Dim header_location As Variant
 
 Sheets("Summary View").Select
 
-Range("A1:K1").Name = Header_Row
+Range("A1:K1").Name = "Header_Row"
 
 For each header in Range("Header_Row")
-
-' Deletes the Concat column
-If header = "Concat" Then
-  header_location = Mid(cell.Address, 2, 1)
-  Columns(header_location & ":" & header_location).Select
-  Selection.Delete Shift:=xlToLeft
-End If
+  ' Deletes the Concat column
+  If header = "Concat" Then
+    header_location = Mid(header.Address, 2, 1)
+    Columns(header_location & ":" & header_location).Select
+    Selection.Delete Shift:=xlToLeft
+  End If
+Next header
 
 ' Delete the extra sheets
 Application.DisplayAlerts = False
