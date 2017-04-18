a53336fc77048e586bcae4245f7dddee11e09b84
Jonnokc <JA052464@cerner.net>
2017-04-18 11:23:12 -0500

    Major Rebuild

--- Validation_Form/Forms/3. SHX/SHX_Setup.vb
+++ Validation_Form/Forms/3. SHX/SHX_Setup.vb
@@ -9,15 +9,27 @@ Dim LastColumn As Long
 Dim StartCell As Range
 Dim rList As Range
 Dim Header_Array As Variant
+Dim Validated_Map_Headers As Variant
+Dim DataRange As Variant
+Dim Result_Check As Boolean
+Dim Header_Check As Boolean
+Dim Start_Check As Integer
 
     'Disables settings to improve performance
-    Application.ScreenUpdating = False
-    Application.Calculation = xlCalculationManual
-    Application.EnableEvents = False
+    ' Application.ScreenUpdating = False
+    ' Application.Calculation = xlCalculationManual
+    ' Application.EnableEvents = False
 
     Header_Array = Array("NOMEN_ID", "CS_72", "CS_14003", "CS_4002165")
+    Validated_Map_Headers = Array("CODE ID", "MAPPING STATUS")
 
-    MsgBox ("Program is about to run. Please leave computer alone until completed")
+    'Prompts user to confirm they have reviewed the data in the validation form BEFORE running this.
+    Start_Check = MsgBox("BORIS is about to run the SHX helper program. This can take a few minutes." & vbNewLine & vbNewLine & "Click OK to start or Cancel to exit. Follow on screen prompts otherwise leave your computer alone until BORIS is done.", vbOKCancel + vbQuestion, "There Can Only Be One BORIS")
+
+    'If user hits cancel then close program.
+    If Start_Check = vbCancel Then
+        GoTo User_Exit
+    End If
 
     Sheets("Social History Results").Select
 
@@ -64,159 +76,219 @@ Dim Header_Array As Variant
         .TintAndShade = 0
     End With
 
-    Sheets("Social History Results").Select
 
+    ' SUB - Finds the locations of the lookup columns on the social history results sheet
+    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    ' Re-enables screen update incase user needs to interact
+    Application.ScreenUpdating = True
+    Application.EnableEvents = True
+
+    Sheets("Social History Results").Select
     Range("A1").Select
     Range(Selection, Selection.End(xlToRight)).Select
     Selection.Name = "Header_Row"
 
+    For i = 0 To UBound(Header_Array)
+      Header_Check = False
+      For Each Header In Range("Header_Row")
+              If LCase(Header_Array(i)) = LCase(Header) Then
+                  Header_Array(i) = Mid(Header.Address, 2, 1)
+                  Header_Check = True
+                  Exit For
+              End If
+        Next Header
+        If Header_Check = False Then
+            Header_User_Response = InputBox("BORIS was unable to find the header:" & vbNewLine &  "'" & Header_Array(i) & "'" & " on the Social History Results Sheet....." & vbNewLine & vbNewLine & "However all is not lost! BORIS and you can do this!" & vbNewLine & vbNewLine & "To resolve the issue BORIS needs you to enter the letter of a column to use in place of the one he couldn't find." & vbNewLine & vbNewLine & "Look at the excel sheet behind this box and enter (in uppercase) the letter of the column you want to use in place of the missing one." & vbNewLine & vbNewLine & "If you don't want to replace data from another column in place of the missing one then enter the letter of an empty column(like T). If you would rather fix the issue within the file or program then click cancel.", "If I am BORIS who are you?")
 
-    ' Finds the locations of the lookup columns
-    For Each Header In Range("Header_Row")
-        For i = 0 To UBound(Header_Array)
-            If Header_Array(i) = Header Then
-                Header_Array(i) = Mid(Header.Address, 2, 1)
-                Exit For
+            'If user hits cancel then close program.
+            If Header_User_Response = vbNullString Then
+                GoTo User_Exit
+            Else
+                Header_Array(i) = Header_User_Response
             End If
-        Next i
-    Next Header
+        End If
+    Next i
 
 
-    ' Checks Validated Mappings Code ID Column to confirm format is NumberFormat
+    ' SUB - Finds the headers for the validated mappings sheet
+    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     Sheets("Validated Mappings").Select
-
     Range("A1").Select
     Range(Selection, Selection.End(xlToRight)).Name = "Val_Headers"
 
-    For Each cell In Range("Val_Headers")
-        If cell = LCase("Code ID") Then
-            CodeID = Mid(cell.Address, 2, 1)
+    For i = 0 To UBound(Validated_Map_Headers)
+    Header_Check = False
+      For Each Header In Range("Val_Headers")
+        If LCase(Validated_Map_Headers(i)) = LCase(Header) Then
+            Validated_Map_Headers(i) = Mid(Header.Address, 2, 1)
+            Header_Check = True
+            Exit For
         End If
-        Exit For
+      Next header
+        If Header_Check = False Then
+            Header_User_Response = InputBox("BORIS was unable to find the header:" & vbNewLine &  "'" & Validated_Map_Headers(i) & "'" & " on the Social History Results Sheet....." & vbNewLine & vbNewLine & "However all is not lost! BORIS and you can do this!" & vbNewLine & vbNewLine & "To resolve the issue BORIS needs you to enter the letter of a column to use in place of the one he couldn't find." & vbNewLine & vbNewLine & "Look at the excel sheet behind this box and enter (in uppercase) the letter of the column you want to use in place of the missing one." & vbNewLine & vbNewLine & "If you don't want to replace data from another column in place of the missing one then enter the letter of an empty column(like T). If you would rather fix the issue within the file or program then click cancel.", "If I am BORIS who are you?")
 
-        ' Sets Code ID range for looping to check format
-        Range(CodeID & "2:" & CodeID & ActiveSheet.Cells.SpecialCells(xlCellTypeLastCell).Row).Name = "CodeID_Column"
+            'If user hits cancel then close program.
+            If Header_User_Response = vbNullString Then
+                GoTo User_Exit
+            Else
+                Validated_Map_Headers(i) = Header_User_Response
+            End If
+        End If
+    Next i
 
-        For Each cell In Range("CodeID_Column")
+    ' Disables screen update again after user interaction part
+    Application.ScreenUpdating = False
+    Application.EnableEvents = False
+
+    ' Sets Code ID range for looping to check format
+    Range(Validated_Map_Headers(0) & "2:" & Validated_Map_Headers(0) & ActiveSheet.Cells.SpecialCells(xlCellTypeLastCell).Row).Name = "CodeID_Column"
+
+    ' Checks the code id column to confirm format
+    For i = 0 to UBound(DataRange)
+        If IsNumeric(DataRange(i)) Then
+            DataRange(i).Value = Val(DataRange(i).Value)
+            DataRange(i).NumberFormat = "0"
+        End If
+    Next i
+
+
+    ' Checks code columns and makes sure values are in number format for lookup
+    For i = 0 To UBound(Header_Array)
+        'Selects all cells not empty in the code column and assigns named range for loop
+        Range(Header_Array(i) & "2:" & Header_Array(i) & ActiveSheet.Cells.SpecialCells(xlCellTypeLastCell).Row).Name = "Code_Column"
+
+        For Each cell In Range("Code_Column")
             If IsNumeric(cell) Then
                 cell.Value = Val(cell.Value)
                 cell.NumberFormat = "0"
             End If
         Next cell
+    Next i
 
 
-        ' Checks code columns and makes sure values are in number format for lookup
-        For i = 0 To UBound(Header_Array)
-            'Selects all cells not empty in the code column and assigns named range for loop
-            Range(Header_Array(i) & "2:" & Header_Array(i) & ActiveSheet.Cells.SpecialCells(xlCellTypeLastCell).Row).Name = "Code_Column"
+    ' CS Nomenclature Mapped
 
-            For Each cell In Range("Code_Column")
-                If IsNumeric(cell) Then
-                    cell.Value = Val(cell.Value)
-                    cell.NumberFormat = "0"
-                End If
-            Next cell
-        Next i
+    Sheets("Social History Results").Range("A2").Formula = _
+            "=IFERROR(INDEX('Validated Mappings'!" & Validated_Map_Headers(1) & ":" & Validated_Map_Headers(1) & ",MATCH(" & Header_Array(0) & "2,'Validated Mappings'!" & Validated_Map_Headers(0) & ":" & Validated_Map_Headers(0) & ",0))," & Header_Array(0) & "2)"
 
+    ' CS 72 formula
+    Sheets("Social History Results").Range("B2").Formula = _
+            "=IFERROR(INDEX('Validated Mappings'!" & Validated_Map_Headers(1) & ":" & Validated_Map_Headers(1) & ",MATCH(" & Header_Array(1) & "2,'Validated Mappings'!" & Validated_Map_Headers(0) & ":" & Validated_Map_Headers(0) & ",0))," & Header_Array(1) & "2)"
 
-        ' CS Nomenclature Mapped
-        Range("A2").Select
-        ActiveCell = "=IFERROR(INDEX('Validated Mappings'!I:I,MATCH(" & Header_Array(0) & "2,'Validated Mappings'!D:D,0))," & Header_Array(0) & "2)"
+    ' CS 14003 Formula
+    Sheets("Social History Results").Range("C2").Formula = _
+            "=IFERROR(INDEX('Validated Mappings'!" & Validated_Map_Headers(1) & ":" & Validated_Map_Headers(1) & ",MATCH(" & Header_Array(2) & "2,'Validated Mappings'!" & Validated_Map_Headers(0) & ":" & Validated_Map_Headers(0) & ",0))," & Header_Array(2) & "2)"
 
-        ' CS 72 formula
-        Range("B2").Select
-        ActiveCell = "=IFERROR(INDEX('Validated Mappings'!I:I,MATCH(" & Header_Array(1) & "2,'Validated Mappings'!D:D,0))," & Header_Array(1) & "2)"
-
-        ' CS 14003 Formula
-        Range("C2").Select
-        ActiveCell = "=IFERROR(INDEX('Validated Mappings'!I:I,MATCH(" & Header_Array(2) & "2,'Validated Mappings'!D:D,0))," & Header_Array(2) & "2)"
-
-        ' CS 4002165
-        Range("D2").Select
-        ActiveCell = "=IFERROR(INDEX('Validated Mappings'!I:I,MATCH(" & Header_Array(3) & "2,'Validated Mappings'!D:D,0))," & Header_Array(3) & "2)"
+    ' CS 4002165
+    Sheets("Social History Results").Range("D2").Formula = _
+            "=IFERROR(INDEX('Validated Mappings'!" & Validated_Map_Headers(1) & ":" & Validated_Map_Headers(1) & ",MATCH(" & Header_Array(3) & "2,'Validated Mappings'!" & Validated_Map_Headers(0) & ":" & Validated_Map_Headers(0) & ",0))," & Header_Array(3) & "2)"
 
 
-        'Centers cell values
-        Columns("A:D").Select
-        With Selection
-            .HorizontalAlignment = xlCenter
-            .Orientation = 0
-            .AddIndent = False
-            .IndentLevel = 0
-            .ShrinkToFit = False
-            .ReadingOrder = xlContext
-            .MergeCells = False
-        End With
+    'Centers cell values
+    Columns("A:D").Select
+    With Selection
+        .HorizontalAlignment = xlCenter
+        .Orientation = 0
+        .AddIndent = False
+        .IndentLevel = 0
+        .ShrinkToFit = False
+        .ReadingOrder = xlContext
+        .MergeCells = False
+    End With
 
-        'Re-enables Auto-calculate for forumlas
-        Application.Calculation = xlCalculationAutomatic
+    'Re-enables Auto-calculate to make sure formulas are accurate
+    Application.Calculation = xlCalculationAutomatic
 
-        Sheets("Social History Results").Select
-        Cells.Select
-        Selection.Copy
-        Sheets("To_Review").Select
-        Range("A1").Select
-        Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
-                :=False, Transpose:=False
-
-        ActiveSheet.AutoFilterMode = False
-
-        'If table exists on this sheet, then convert to range
-        If ActiveSheet.ListObjects.Count > 0 Then
-
-            With ActiveSheet.ListObjects(1)
-                Set rList = .Range
-                .Unlist        ' convert the table back to a range
-            End With
-
-            With rList
-                .Interior.ColorIndex = xlColorIndexNone
-                .Font.ColorIndex = xlColorIndexAutomatic
-                .Borders.LineStyle = xlLineStyleNone
-            End With
-
+    ' SUB - If the Review sheet does not exist (ie, working with an old file, then create the sheet)
+    ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    For Each Sheet In Worksheets
+        Result_Check = False
+        If Sheet.Name = "To_Review" Then
+            Result_Check = True
+            Exit For
         End If
+    Next Sheet
+
+    If Result_Check = False Then
+        With ThisWorkbook
+            .Sheets.Add(After:=.Sheets(.Sheets.Count)).Name = "To_Review"
+        End With
+    End If
+
+    ' SUB - Copies the data to the To_Review Sheet
+    ' Disables calculation again
+    Application.Calculation = xlCalculationManual
+    Sheets("Social History Results").Cells.Select
+    Selection.Copy Sheets("To_Review").Range("A1")
 
 
-        Set sht = Worksheets("To_Review")
-        Set StartCell = Range("A1")
+    ' SUB - Formats the data on the To Review Sheet
+    '''''''''''''''''''''''''''''''''''''''''''''''
+    Sheets("To_Review").AutoFilterMode = False
 
-        'Refresh UsedRange
-        Worksheets("Social History Results").UsedRange
+    'If table exists on this sheet, then convert to range
+    If Sheets("To_Review").ListObjects.Count > 0 Then
 
-        'Find Last Row and Column
-        LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
-        LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
-
-        'Select Range
-        sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
-
-        Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
-        tbl.Name = "SHX_To_Review"
-        tbl.TableStyle = "TableStyleLight9"
-
-        Cells.Select
-        Cells.EntireColumn.AutoFit
-        Cells.Select
-        Cells.EntireRow.AutoFit
-
-        Columns("A:D").Select
-        With Selection
-            .HorizontalAlignment = xlCenter
-            .VerticalAlignment = xlCenter
-            .Orientation = 0
-            .AddIndent = False
-            .IndentLevel = 0
-            .ShrinkToFit = False
-            .ReadingOrder = xlContext
-            .MergeCells = False
+        With Sheets("To_Review").ListObjects(1)
+            Set rList = .Range
+            .Unlist
         End With
 
-        Range("A1").Select
+        With rList
+            .Interior.ColorIndex = xlColorIndexNone
+            .Font.ColorIndex = xlColorIndexAutomatic
+            .Borders.LineStyle = xlLineStyleNone
+        End With
 
-        Application.ScreenUpdating = True
-        Application.EnableEvents = True
+    End If
 
-        MsgBox ("Program Completed")
+    Set sht = Worksheets("To_Review")
+    Set StartCell = Range("A1")
 
-    End Sub
+    Worksheets("Social History Results").UsedRange
+
+    LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
+    LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
+
+    sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
+
+    Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
+    tbl.Name = "SHX_To_Review"
+    tbl.TableStyle = "TableStyleLight9"
+
+    Cells.Select
+    Cells.EntireColumn.AutoFit
+    Cells.Select
+    Cells.EntireRow.AutoFit
+
+    Columns("A:D").Select
+    With Selection
+        .HorizontalAlignment = xlCenter
+        .VerticalAlignment = xlCenter
+        .Orientation = 0
+        .AddIndent = False
+        .IndentLevel = 0
+        .ShrinkToFit = False
+        .ReadingOrder = xlContext
+        .MergeCells = False
+    End With
+
+    Range("A1").Select
+
+    Application.ScreenUpdating = True
+    Application.EnableEvents = True
+    Application.Calculation = xlCalculationAutomatic
+
+    MsgBox ("All Done!")
+
+    Exit Sub
+
+    User_Exit:
+      Application.ScreenUpdating = True
+      Application.EnableEvents = True
+      Application.Calculation = xlCalculationAutomatic
+
+      MsgBox ("Program quitting per user action.")
+
+End Sub
