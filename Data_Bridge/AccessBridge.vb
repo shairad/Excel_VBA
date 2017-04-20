3903e03c8b7fd87a8e27ae2bd35455b53b886c92
Jonnokc <JA052464@cerner.net>
2017-04-20 16:37:45 -0500

    Added Code to pull data

---
+++ Data_Bridge/AccessBridge.vb
@@ -0,0 +1,34 @@
+Sub BlacklistImport()
+'
+' access Macro
+'
+
+' PRIMARY - Imports the blacklist code table to the excel sheet for review
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    With ThisWorkbook
+      .Sheets.Add(After:=.Sheets(.Sheets.Count)).Name = "BlackList_Table"
+    End With
+
+    With ActiveSheet.ListObjects.Add(SourceType:=0, Source:=Array( _
+        "OLEDB;Provider=Microsoft.ACE.OLEDB.12.0;Password="""";User ID=Admin;Data Source=Y:\Data Intelligence\Code_Submittion_Database\CodeFeedba" _
+        , _
+        "ckDatabase.accdb;Mode=Share Deny Write;Extended Properties="""";Jet OLEDB:System database="""";Jet OLEDB:Registry Path="""";Jet OLEDB:" _
+        , _
+        "Database Password=BORIS;Jet OLEDB:Engine Type=6;Jet OLEDB:Database Locking Mode=0;Jet OLEDB:Global Partial Bulk Ops=2;Jet OLEDB:" _
+        , _
+        "Global Bulk Transactions=1;Jet OLEDB:New Database Password="""";Jet OLEDB:Create System Database=False;Jet OLEDB:Encrypt Database=" _
+        , _
+        "False;Jet OLEDB:Don't Copy Locale on Compact=False;Jet OLEDB:Compact Without Replica Repair=False;Jet OLEDB:SFP=False;Jet OLEDB:" _
+        , _
+        "Support Complex Data=False;Jet OLEDB:Bypass UserInfo Validation=False;Jet OLEDB:Limited DB Caching=False;Jet OLEDB:Bypass Choice" _
+        , "Field Validation=False"), Destination:=Range("$A$1")).QueryTable
+        .CommandType = xlCmdTable
+        .CommandText = Array("BlacklistedCodeSystems")
+        .RefreshStyle = xlInsertDeleteCells
+        .SourceDataFile = _
+        "Y:\Data Intelligence\Code_Submittion_Database\CodeFeedbackDatabase.accdb"
+        .ListObject.DisplayName = "Table_CodeFeedbackDatabase.accdb"
+        .Refresh BackgroundQuery:=False
+    End With
+End Sub
