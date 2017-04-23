eef5689eacc4330ec1e14c941ede16d58d7a4e8b
Jonnokc <JA052464@cerner.net>
2017-04-22 19:32:15 -0500

    Adjusted layout

--- Data_Bridge/AccessBridge.vb
+++ Data_Bridge/AccessBridge.vb
@@ -3,46 +3,46 @@ Sub BlacklistImport()
 Dim sheet As Worksheet
 
 
-' Checks to see if current workbook has the blacklist sheet and if it does, delete it then remake with new data.
+    ' Checks to see if current workbook has the blacklist sheet and if it does, delete it then remake with new data.
 
 
-Application.DisplayAlerts = False
+    Application.DisplayAlerts = False
 
-For Each sheet In Worksheets
-    If sheet.Name = "BlackList_Table" _
-       Then
-        sheet.Delete
-    End If
-Next sheet
+    For Each sheet In Worksheets
+        If sheet.Name = "BlackList_Table" _
+                Then
+            sheet.Delete
+        End If
+    Next sheet
 
-Application.DisplayAlerts = True
+    Application.DisplayAlerts = True
 
-With ThisWorkbook
-  .Sheets.Add(After:=.Sheets(.Sheets.Count)).Name = "BlackList_Table"
-End With
+    With ThisWorkbook
+        .Sheets.Add(After:=.Sheets(.Sheets.Count)).Name = "BlackList_Table"
+    End With
 
 
     ' PRIMARY - Imports the blacklist code table to the excel sheet for review
     '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
     With ActiveSheet.ListObjects.Add(SourceType:=0, Source:=Array( _
-        "OLEDB;Provider=Microsoft.ACE.OLEDB.12.0;Password="""";User ID=Admin;Data Source=Y:\Data Intelligence\Code_Submittion_Database\CodeFeedba" _
-        , _
-        "ckDatabase.accdb;Mode=Share Deny Write;Extended Properties="""";Jet OLEDB:System database="""";Jet OLEDB:Registry Path="""";Jet OLEDB:" _
-        , _
-        "Database Password=BORIS;Jet OLEDB:Engine Type=6;Jet OLEDB:Database Locking Mode=0;Jet OLEDB:Global Partial Bulk Ops=2;Jet OLEDB:" _
-        , _
-        "Global Bulk Transactions=1;Jet OLEDB:New Database Password="""";Jet OLEDB:Create System Database=False;Jet OLEDB:Encrypt Database=" _
-        , _
-        "False;Jet OLEDB:Don't Copy Locale on Compact=False;Jet OLEDB:Compact Without Replica Repair=False;Jet OLEDB:SFP=False;Jet OLEDB:" _
-        , _
-        "Support Complex Data=False;Jet OLEDB:Bypass UserInfo Validation=False;Jet OLEDB:Limited DB Caching=False;Jet OLEDB:Bypass Choice" _
-        , "Field Validation=False"), Destination:=Range("$A$1")).QueryTable
+            "OLEDB;Provider=Microsoft.ACE.OLEDB.12.0;Password="""";User ID=Admin;Data Source=Y:\Data Intelligence\Code_Submittion_Database\CodeFeedba" _
+            , _
+            "ckDatabase.accdb;Mode=Share Deny Write;Extended Properties="""";Jet OLEDB:System database="""";Jet OLEDB:Registry Path="""";Jet OLEDB:" _
+            , _
+            "Database Password=BORIS;Jet OLEDB:Engine Type=6;Jet OLEDB:Database Locking Mode=0;Jet OLEDB:Global Partial Bulk Ops=2;Jet OLEDB:" _
+            , _
+            "Global Bulk Transactions=1;Jet OLEDB:New Database Password="""";Jet OLEDB:Create System Database=False;Jet OLEDB:Encrypt Database=" _
+            , _
+            "False;Jet OLEDB:Don't Copy Locale on Compact=False;Jet OLEDB:Compact Without Replica Repair=False;Jet OLEDB:SFP=False;Jet OLEDB:" _
+            , _
+            "Support Complex Data=False;Jet OLEDB:Bypass UserInfo Validation=False;Jet OLEDB:Limited DB Caching=False;Jet OLEDB:Bypass Choice" _
+            , "Field Validation=False"), Destination:=Range("$A$1")).QueryTable
         .CommandType = xlCmdTable
         .CommandText = Array("BlacklistedCodeSystems")
         .RefreshStyle = xlInsertDeleteCells
         .SourceDataFile = _
-        "Y:\Data Intelligence\Code_Submittion_Database\CodeFeedbackDatabase.accdb"
+                "Y:\Data Intelligence\Code_Submittion_Database\CodeFeedbackDatabase.accdb"
         .ListObject.DisplayName = "UnmappedList"
         .Refresh BackgroundQuery:=False
     End With
