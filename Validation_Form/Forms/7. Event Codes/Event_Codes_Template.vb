873314932315425d2c068c1857a1b0815b41ea59
Shai Rado <shairad@gmail.com>
2017-04-26 17:02:33 +0300

    Updated code to remove all `Select` and added variable for the PivotTable
    I have changed your code everywhen you has `Select` since it's slows down your code's run-time. 
    
    Also, added variables to support `PivotTable`.

--- Validation_Form/Forms/7. Event Codes/Event_Codes_Template.vb
+++ Validation_Form/Forms/7. Event Codes/Event_Codes_Template.vb
@@ -1,99 +1,80 @@
 Sub EV_Code_Setup()
+    
+    Dim tbl As ListObject    
+    Dim sht As Worksheet    
+    Dim LastRow As Long    
+    Dim LastColumn As Long    
+    Dim StartCell As Range    
+    Dim PvtTbl As PivotTable
+    
+    Application.ScreenUpdating = False        
+    
+    Set sht = Worksheets("Event Codes Results")    
+    With sht   ' replaced 'Select' which slows down your code with `With` Statement        
+        If .ListObjects.Count > 0 Then            
+            With .ListObjects(1)                
+                Set rList = .Range                
+                .Unlist                           ' convert the table back to a range            
+            End With
+            With rList                
+                .Interior.ColorIndex = xlColorIndexNone                
+                .Font.ColorIndex = xlColorIndexAutomatic                
+                .Borders.LineStyle = xlLineStyleNone            
+            End With  
+        End If                
+        Set StartCell = .Range("A1")
+        'Refresh UsedRange        
+    '   .UsedRange ' *** <-- Not sure what you are trying to achieve with this line ?     
 
-    Dim tbl As ListObject
-    Dim sht As Worksheet
-    Dim LastRow As Long
-    Dim LastColumn As Long
-    Dim StartCell As Range
+        'Find Last Row and Column        
+        LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row        
+        LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column            
 
-    Application.ScreenUpdating = False
+        ' Edited : Turn Range Into Table (without Selection)        
+        Set tbl = .ListObjects.Add(xlSrcRange, .Range(StartCell, .Cells(LastRow, LastColumn)), , xlYes)        
+        tbl.Name = "EV_Results_Table"        
+        tbl.TableStyle = "TableStyleLight9"            
+    
+        'changes font color of header row to white        
+        With .Rows("1:1").Font            
+            .ThemeColor = xlThemeColorDark1           
+            .TintAndShade = 0        
+        End With            
 
-    Sheets("Event Codes Results").Select
-
-    If ActiveSheet.ListObjects.Count > 0 Then
-
-        With ActiveSheet.ListObjects(1)
-            Set rList = .Range
-            .Unlist                           ' convert the table back to a range
-        End With
-
-        With rList
-            .Interior.ColorIndex = xlColorIndexNone
-            .Font.ColorIndex = xlColorIndexAutomatic
-            .Borders.LineStyle = xlLineStyleNone
-        End With
-
-    End If
-
-    Set sht = Worksheets("Event Codes Results")
-    Set StartCell = Range("A1")
-
-    'Refresh UsedRange
-    Worksheets("Event Codes Results").UsedRange
-
-    'Find Last Row and Column
-    LastRow = StartCell.SpecialCells(xlCellTypeLastCell).Row
-    LastColumn = StartCell.SpecialCells(xlCellTypeLastCell).Column
-
-    'Select Range
-    sht.Range(StartCell, sht.Cells(LastRow, LastColumn)).Select
-
-    'Turn selected Range Into Table
-    Set tbl = ActiveSheet.ListObjects.Add(xlSrcRange, Selection, , xlYes)
-    tbl.Name = "EV_Results_Table"
-    tbl.TableStyle = "TableStyleLight9"
-
-    'changes font color of header row to white
-    Rows("1:1").Select
-    With Selection.Font
-        .ThemeColor = xlThemeColorDark1
-        .TintAndShade = 0
+        .Range("A2") = "=IFERROR(INDEX('Validated Codes'!I:I,MATCH(D3,'Validated Codes'!D:D,0)),0)"        
+        .Range("A2").AutoFill Destination:=Range("EV_Results_Table[Mapped?]")    
     End With
 
-    Range("A2").Select
+    ActiveWorkbook.PivotCaches.Create(SourceType:=xlDatabase, SourceData:= _   
+        "EV_Results_Table", Version:=6).CreatePivotTable TableDestination:= _       
+        "'Pivot Table'!R12C2", TableName:="EV_Pivot", DefaultVersion:=6       
 
-    Sheets("Event Codes Results").Select
-    Range("A2").Select
-    ActiveCell = "=IFERROR(INDEX('Validated Codes'!I:I,MATCH(D3,'Validated Codes'!D:D,0)),0)"
-    Selection.AutoFill Destination:=Range("EV_Results_Table[Mapped?]")
-    Range("A2").Select
-
-    'Creates and formats Pivot TableStyle
-
-    Sheets("Event Codes Results").Select
-
-    ActiveWorkbook.PivotCaches.Create(SourceType:=xlDatabase, SourceData:= _
-                                      "EV_Results_Table", Version:=6).CreatePivotTable TableDestination:= _
-                                      "'Pivot Table'!R12C2", TableName:="EV_Pivot", DefaultVersion:=6
-    Sheets("Pivot Table").Select
-    Cells(12, 2).Select
-    With ActiveSheet.PivotTables("EV_Pivot").PivotFields("Mapped?")
-        .Orientation = xlRowField
-        .Position = 1
+    ' set the Pivot Table "EV_Pilot" to an Object   
+    Set PvtTbl = Sheets("Pivot Table").PivotTables("EV_Pilot")   
+    With PvtTbl       
+        With .PivotFields("Mapped?")        
+            .Orientation = xlRowField           
+            .Position = 1        
+        End With        
+        With .PivotFields("CODE_STATUS")            
+            .Orientation = xlColumnField           
+            .Position = 1        
+        End With       
+        ' add field to Pivot Table as Count        
+        .AddDataField .PivotFields("EVENT_CD"), "Count of EVENT_CD", xlCount    
     End With
-    With ActiveSheet.PivotTables("EV_Pivot").PivotFields("CODE_STATUS")
-        .Orientation = xlColumnField
-        .Position = 1
-    End With
-    ActiveSheet.PivotTables("EV_Pivot").AddDataField ActiveSheet.PivotTables( _
-                                                     "EV_Pivot").PivotFields("EVENT_CD"), "Count of EVENT_CD", xlCount
-
-    'Selects the Validated count and changes color to red
-    Range("C15").Select
-    With Selection.Font
-        .Color = -16776961
-        .TintAndShade = 0
+    
+    'Selects the Validated count and changes color to red    
+    With Sheets("Pivot Table").Range("C15").Font        
+        .Color = -16776961        
+        .TintAndShade = 0    
     End With
 
-
-    Sheets("Event Codes Results").Select
-    'Filters the results table column A for just "Validated"
-    ActiveSheet.ListObjects("EV_Results_Table").Range.AutoFilter Field:=1, _
-                                                                 Criteria1:="Validated"
-    'Filters the Code_Status column for just "Active"
-    ActiveSheet.ListObjects("EV_Results_Table").Range.AutoFilter Field:=3, _
-                                                                 Criteria1:="Active"
-
-    Application.ScreenUpdating = True
+    With tbl ' <-- you already defined it, so you can use this Object        
+        'Filters the results table column A for just "Validated"       
+        .Range.AutoFilter Field:=1, Criteria1:="Validated"                'Filters the Code_Status column for just "Active"       
+        .Range.AutoFilter Field:=3, Criteria1:="Active"    
+    End With
+    Application.ScreenUpdating = True
 
 End Sub
